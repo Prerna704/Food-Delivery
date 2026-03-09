1c8aad6f2b23d9145c81c82d201d05c55d0423bb
Prerna704 <prernaprem720@gmail.com>
2026-03-09 12:05:58 +0530

    changed admin name

--- admin/src/components/Login/Login.jsx
+++ admin/src/components/Login/Login.jsx
@@ -22,7 +22,7 @@ const Login = ({ url }) => {
     event.preventDefault();
     const response = await axios.post(url + "/api/user/login", data);
     if (response.data.success) {
-      if (response.data.role === "admin") {
+      if (response.data.role === "admin1") {
         setToken(response.data.token);
         setAdmin(true);
         localStorage.setItem("token", response.data.token);
